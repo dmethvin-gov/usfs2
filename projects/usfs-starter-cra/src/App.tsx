ae3add8b6bedb43f2fad7087558bbdaa30691c37
Dave Methvin <David.W.Methvin@omb.eop.gov>
2018-12-07 10:15:18 -0500

    Convert to TypeScript

---
+++ projects/usfs-starter-cra/src/App.tsx
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+//import 'uswds-widgets/dist/lib/css/styles.css';
+
+// Typical header and footer just to show how it would be used
+// Normally we'd import these from somewhere
+function SiteHeader() {
+  return <div>SITE HEADER</div>;
+}
+function SiteFooter() {
+  return <div>SITE FOOTER</div>;
+}
+
+class App extends Component {
+  render() {
+    return (
+      <>
+        <SiteHeader/>
+        <div>content</div>
+        <SiteFooter/>
+      </>
+    );
+  }
+}
+
+export default App;
